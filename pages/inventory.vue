e841d4a29f992d337ba2e105c57bd9610809dc79
ronaldjohnatanoso <ronaldjohnatanoso@gmail.com>
2024-04-18 22:41:17 +0800

    store location autocomplete

--- pages/inventory.vue
+++ pages/inventory.vue
@@ -1,14 +1,113 @@
 <template>
-    <div>
-        this is inventory
-    </div>
+    <v-container>
+        Enter the location of the store:
+    </v-container>
+    <v-container>
+        <v-autocomplete
+  label="Enter store location"
+    v-model="pickedLocation"
+  :items="locationArray"
+></v-autocomplete>
+        <v-container>
+            Store Location: {{ pickedLocation }}
+        </v-container>
+    </v-container>
+    <v-table lazy density="compact">
+        <thead>
+            <tr>
+                <th class="text-left">
+                    Product ID
+                </th>
+                <th class="text-left">
+                    Product Name
+                </th>
+                <th class="text-left">
+                    Quantity
+                </th>
+                <th class="text-left">
+                    Ordered At Time:
+                </th>
+                <th class="text-left">
+                    Price:
+                </th>
+                <th class="text-left">
+                    Delete Order
+                </th>
+                <th class="text-left">
+                    Remaining Stock
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- <tr v-for="item in orderArray" :key="item.id">
+                <td>{{ item.Product.id }}</td>
+                <td>{{ item.Product.name }}</td>
+                <td>{{ item.quantity }}</td>
+                <td>{{ item.created_at }}</td>
+                <td>{{ item.Product.price }}</td>
+   
+                <td>{{ item.Product.remaining_stock }}</td>
+            </tr> -->
+        </tbody>
+    </v-table>
 </template>
 
 <script setup lang="ts">
+import type { Database } from '~/lib/database.types';
+
 definePageMeta({
     middleware: ["profile-auth"],
     layout: "topbar-layout",
 })
+
+const supabase = useSupabaseClient()
+const storeArray = ref<Database['public']['Tables']['Store']['Row'][]>([])
+const locationArray = ref<string[]>([])
+const pickedLocation = ref<string | null>(null)
+const handleFetchStores = async () => {
+    try {
+        const {data, error} = await supabase
+            .from('Store')
+            .select('*')
+
+        if( error) throw error
+        if(!error) {
+            console.log(data)
+            storeArray.value = data
+        }
+
+        //get the locations only from the object
+        locationArray.value = storeArray.value
+            .filter(store => store.location !== null)
+            .map(store => store.location as string);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const handleFetchInventory = async () => {
+    console.log("i fetch inventory" + pickedLocation.value)
+    try {
+        // const {data, error} = await supabase
+        //     .from('Inventory')
+        //     .select('*')
+        //     .eq('store_location', pickedLocation.value)
+
+        // if( error) throw error
+        // if(!error) {
+        //     console.log(data)
+        //     // storeArray.value = data
+        // }
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+handleFetchStores()
+watch(pickedLocation, handleFetchInventory)
+
 </script>
 
 <style scoped>
