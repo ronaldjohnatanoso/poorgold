bc027e078993215937bf291c7fbab4daf7e15338
ronaldjohnatanoso <ronaldjohnatanoso@gmail.com>
2024-05-01 19:08:09 +0800

    home page styling and scroll into view

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,82 @@
 <template>
+    <!-- <img src="/images/png/abstract-green-background-free-vector.jpg" alt=""> -->
 
-    <div class="page-container">
+
+
+
+    <div  class="page-container mb-[-25px]">
     
         <div class="p-2 flex justify-center">
             <div class="font-bold text-2xl">Shop now</div>
         </div>
         <v-container>(refresh if no products show)</v-container>
-        <v-container class="shadow-xl bg-yellow-100">
-            <v-autocomplete @update:modelValue="handleTypeChange" hide-details single-line variant="outlined"
-                prepend-inner-icon="mdi-magnify" v-model="selectedProductType" label="Product Type"
-                :items="categoryList"></v-autocomplete>
-                <v-container>
-                    <v-row>
-                        <v-col v-for="type in categoryList">
-                            <v-btn @click="handleTypeChange(type)" color="green">{{type}}</v-btn>
-                        </v-col>
-                    </v-row>
-                </v-container>
+        <v-container class="bg-yellow-100 rounded-md shadow-2xl">
+            <v-container>
+                <span  ref="productTypeContainer"></span>
+                <v-autocomplete @update:modelValue="handleTypeChange" hide-details single-line variant="outlined"
+                    prepend-inner-icon="mdi-magnify" v-model="selectedProductType" label="Product Type"
+                    :items="categoryList"></v-autocomplete>
+                    <v-container>
+                        <v-row>
+                            <v-col v-for="type in categoryList">
+                                <v-btn @click="handleTypeChange(type)" color="green">{{type}}</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+       
+            </v-container>
+        
+         <v-container class=" ">
+                <v-container  v-if="selectedProductType"><span class="font-bold">Category: {{ selectedProductType  }}</span></v-container>
+                <v-row dense  >
+                    <Product :productData="product" v-for="product in filteredProducts" />
+                </v-row> 
+            </v-container>  
         </v-container>
-    
-     <v-container   class="shadow-xl rounded bg-yellow-100">
-            <v-container  v-if="selectedProductType"><span class="font-bold">Category: {{ selectedProductType  }}</span></v-container>
-            <v-row dense  >
-                <Product :productData="product" v-for="product in filteredProducts" />
-            </v-row> 
-        </v-container>  
     
         <br>
-        <v-container class="shadow-xl bg-blue-200">
-            <v-autocomplete @update:modelValue="handleNameChange" hide-details single-line variant="outlined" prepend-inner-icon="mdi-magnify"
-                v-model="selectedProductName" label="Search Product Name" :items="allProductNameList"></v-autocomplete>
+        <v-container class="shadow-2xl rounded-md bg-blue-200 ">
+            <v-container class="">
+                <span ref="browseAllContainer"></span>
+                <v-autocomplete @update:modelValue="handleNameChange" hide-details single-line variant="outlined" prepend-inner-icon="mdi-magnify"
+                    v-model="selectedProductName" label="Search Product Name" :items="allProductNameList"></v-autocomplete>
+            </v-container>
+            <v-container v-if="searchedInventoryProduct.length < 1" :key="refreshKey" >
+                <v-row dense>
+                    <v-container><span class="font-bold">All Products</span></v-container>
+                    <Product class="my-5" :productData="product" v-for="product in inventoryProductsArray" />
+                </v-row>
+                
+            </v-container>
+        
+            <v-container v-else-if="searchedInventoryProduct.length > 0" ">
+                <v-row dense>
+                    <v-container>Browse All</v-container>
+                    <Product class="my-5" :productData="product" v-for="product in searchedInventoryProduct" />
+                </v-row>
+            </v-container>
         </v-container>
-        <v-container v-if="searchedInventoryProduct.length < 1" :key="refreshKey" class="shadow-xl rounded bg-blue-200">
-            <v-row dense>
-                <v-container><span class="font-bold">All Products</span></v-container>
-                <Product class="my-5" :productData="product" v-for="product in inventoryProductsArray" />
-            </v-row>
-        </v-container>
-    
-        <v-container v-else-if="searchedInventoryProduct.length > 0"  class="shadow-xl rounded bg-blue-200">
-            <v-row dense>
-                <v-container>Browse All</v-container>
-                <Product class="my-5" :productData="product" v-for="product in searchedInventoryProduct" />
-            </v-row>
-        </v-container>
+ 
     
     </div>
     
     
     
     
-    <v-bottom-navigation>
-        <v-btn value="recent">
-          <v-icon>mdi-history</v-icon>
+    <v-bottom-navigation class="nice">
+        <v-btn @click="handleViewCategory" value="category">
+          <v-icon>mdi-star</v-icon>
       
-          <span>Recent</span>
+          <span>Categories</span>
         </v-btn>
       
-        <v-btn value="favorites">
+        <v-btn @click="handleViewAll" value="all">
           <v-icon>mdi-heart</v-icon>
       
-          <span>Favorites</span>
+          <span>Browse All</span>
         </v-btn>
       
-        <v-btn value="nearby">
-          <v-icon>mdi-map-marker</v-icon>
-      
-          <span>Nearby</span>
-        </v-btn>
+
       </v-bottom-navigation>
     </template>
     
@@ -86,7 +95,7 @@
     
     const refreshKey = ref(0)
     import type { InventoryProduct } from '~/lib/table';
-    
+    //import backgroundImage from '@/assets/images/abstract-green-background-free-vector.jpg';
     
     const supabase = useSupabaseClient<Database>();
     const inventoryProductStore = await useInventoryProductStore()
@@ -98,9 +107,12 @@
     const selectedProductName = ref<string | null>(null)
     const selectedProductType = ref<string | null>(null)
     const searchedInventoryProduct = ref<InventoryProduct[]>([])
-    
+    const productTypeContainer = ref<HTMLElement | null>(null)
+    const browseAllContainer = ref<HTMLElement | null>(null)
     //filter products that only have the selected product type
     const filteredProducts = ref<InventoryProduct[]>([])
+    
+    import bg_green from '@/assets/bg-green.jpg'
     
     const fetchCategories = async () => {
         const { data, error } = await supabase.from(`Inventory`)
@@ -144,6 +156,20 @@
     
     
     }
+
+    const handleViewCategory = () => {
+    console.log("view category");
+    if (productTypeContainer.value instanceof HTMLElement) {
+        productTypeContainer.value.scrollIntoView({ behavior: 'smooth' });
+    }
+};
+
+const handleViewAll = () => {
+    console.log("view all")
+    if (browseAllContainer.value instanceof HTMLElement) {
+        browseAllContainer.value.scrollIntoView({ behavior: 'smooth'});
+    }
+}
     
     const handleNameChange = (value: string) => {
          selectedProductName.value = value
@@ -169,10 +195,17 @@
     </script>
     
     <style scoped>
-        .page-containedr{
+        .page-container{
     
-            background: linear-gradient(to bottom right, #4facfe, #00f2fe);
+            background-image: url('/images/png/abstract-green-background-free-vector.jpg');
+            background-repeat: repeat;
+            background-size: cover;
+            margin-top: -1rem;
+;
+        }
     
+        .nice {
+            background: linear-gradient(to right, #dae699, hsl(55, 72%, 79%));
         }
     
     </style>
